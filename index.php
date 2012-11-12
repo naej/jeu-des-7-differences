46a30e706381b0b9a2c6449913bfb59b7b6aa122
anthony134 <Anthony@10.25.54.115>
2012-11-12 12:46:42 +0100

    Architecture

--- index.php
+++ index.php
@@ -5,8 +5,50 @@
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+         <div id="acceuil">
+        </div>
+        <div id="contact">
+        </div>
+        <div id="aide">
+        </div>
+        
+  
+
+        <div id="indices" class="popup_block">
+            Indices 
+            <br>
+            <article><aside>Indice 1</aside>
+
+            </article>
+            <article><aside>Indice 2</aside>
+            
+            </article>
+            <article><aside>Indice 3</aside>
+            
+            </article>
+        </div>
+        <div id="jeu">
+        </div>
+        <div id="comparer">
+            <img src="image/viergeOriginal.png"> 
+            <img src="image/viergeEnCours.png"> 
+            <br />
+            <button>Fermer</button>
+        </div>
+        <div id="abandonner">
+            Voulez-vous quitter la partie en cours ?
+            <br>
+            <button>Oui</button>
+            <button>Non</button>
+        </div>
+        <div id="victoire">
+            Victoire vous avez gagné 
+            <br>
+            <button>Rejouer</button>
+            <button>Quitter</button>
+        </div>
+        <div id="viergeComplete">
+            Vous avez trouvé toutes les erreurs de cette vierge  
+        </div>
     </body>
 </html>
