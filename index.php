38de06a9a0dfa8e4635c49ec042d4e95f36f7274
anthony134 <Anthony@10.25.54.115>
2012-11-15 18:06:44 +0100

    Ajout fonction Comparer, abandonne, victoire

--- index.php
+++ index.php
@@ -2,19 +2,123 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+           <link rel="stylesheet" href="index.css" type="text/css" media="screen">
+        <script type="text/javascript" src="jquery-ui/index.html"></script>
+        
+        
+        
+        <link href="./jquery-ui/css/smoothness/jquery-ui-1.9.1.custom.css" rel="stylesheet" type="text/css"/>
+        <link href="./jquery-ui/css/smoothness/jquery-ui-1.9.1.custom.min.css" rel="stylesheet" type="text/css"/>
+  
+        
+        <script type="text/javascript" src="./jquery-ui/js/jquery-1.8.2.js"></script>
+        <script type="text/javascript" src="./jquery-ui/js/jquery-1.9.1.custom.js"></script>
+        <script type="text/javascript" src="./jquery-ui/js/jquery-ui-1.9.1.custom.min.js"></script>
+        
+        
+        <script type="text/javascript">
+        $(document).ready(function () {
+            //<script type="text/javascript" src="index.js">
+            //$("#yourDiv").dialog({closeText:'hide'});
+            //$('#yourDiv').dialog({modal:true}); // setup dialog. 
+            //$('#yourDiv').dialog('open');//open dialog
+            
+            
+            //Fonction comparer
+            $("#bt_comparer").click(function(){
+                $("#comparer").dialog('open');
+            });
+            $( "#comparer" ).dialog({
+                autoOpen: false,
+                resizable: false,
+                width: 400,
+                buttons: [{
+                    text: "Fermer",
+                    click: function() {
+                        $( this ).dialog( "close" );
+                        }
+                    }]
+            });
+            $('#comparer').dialog({modal:true}); // setup dialog. 
+           // $('#comparer').dialog('open');//open dialog
+      
+
+           //Fonction Abandonner
+            $("#bt_abandonner").click(function(){
+                $("#abandonner").dialog('open');
+            });
+            $( "#abandonner" ).dialog({
+                autoOpen: false,
+                resizable: false,
+                modal:true,
+                width: 400,
+                buttons: [{
+                    text: "Oui",
+                    click: function() {
+                        opener=self; 
+                        self.close(); 
+                        }
+                    },
+                    {
+                        text: "Non", 
+                        click: function(){
+                            $(this).dialog("close");
+                        }
+                    }]
+            });
+        
+        
+            //Fonction Victoire
+            //A modifier Pour activer l'événement lorsque toutes les vierges ont été corrigés
+            $("#bt_victoire").click(function(){
+                $("#victoire").dialog('open');
+            });
+            $( "#victoire" ).dialog({
+                autoOpen: false,
+                resizable: false,
+                modal:true,
+                width: 400,
+                buttons: [{
+                        text: "Rejouer", 
+                        click: function(){
+                            $(this).dialog("close");
+                            //Ajouter le fait de rejouer 
+                        }
+                    },{
+                        text: "Quitter",
+                        click: function() {
+                            opener=self; 
+                            self.close(); 
+                            }
+                    }]
+            });
+ });
+        
+          
+        </script>
+        
         <title></title>
     </head>
     <body>
-         <div id="acceuil">
-        </div>
-        <div id="contact">
-        </div>
-        <div id="aide">
+        
+        <button id="bt_comparer">Comparer</button>
+        <div id="comparer" title="Comparaison des deux vierges">
+            <img src="image/viergeOriginal.png"> 
+            <img src="image/viergeEnCours.png">
         </div>
         
-  
+        <button id="bt_abandonner">Abandonner</button>
+        <div id="abandonner" title="Abandon">
+            Voulez-vous quitter la partie en cours ?
+        </div>
+        
+        
+        <button id="bt_victoire" title="Victoire">Victoire</button>
+        <div id="victoire">
+            Victoire vous avez gagné 
+        </div>
 
-        <div id="indices" class="popup_block">
+        <!--<div id="indices" class="popup_block">
             Indices 
             <br>
             <article><aside>Indice 1</aside>
@@ -29,26 +133,8 @@
         </div>
         <div id="jeu">
         </div>
-        <div id="comparer">
-            <img src="image/viergeOriginal.png"> 
-            <img src="image/viergeEnCours.png"> 
-            <br />
-            <button>Fermer</button>
-        </div>
-        <div id="abandonner">
-            Voulez-vous quitter la partie en cours ?
-            <br>
-            <button>Oui</button>
-            <button>Non</button>
-        </div>
-        <div id="victoire">
-            Victoire vous avez gagné 
-            <br>
-            <button>Rejouer</button>
-            <button>Quitter</button>
-        </div>
         <div id="viergeComplete">
             Vous avez trouvé toutes les erreurs de cette vierge  
-        </div>
+        </div>-->
     </body>
 </html>
